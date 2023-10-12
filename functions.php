1b7dcf05846e8cd0226619e5989992f025b3c178
Tinfou <144536026+Tinfou@users.noreply.github.com>
2023-10-12 21:30:56 +0200

    ajout fonction ucfirst prenom

--- functions.php
+++ functions.php
@@ -1,28 +1,29 @@
 <?php 
 
-declare(strict_types=1);
+    declare(strict_types=1);
 
-function getFullName(String $nom, ?string $prenom): string
-    {   
-        if(validationChar($nom) && validationChar($prenom)){
-            $nom = mb_strtoupper($nom);
-            return "votre nom complet est: $nom $prenom"; 
+    function getFullName(String $nom, ?string $prenom): string
+        {   
+            if(validationChar($nom) && validationChar($prenom)){
+                $nom = mb_strtoupper($nom);
+                $prenom = ucfirst($prenom);
+                return "votre nom complet est: $nom $prenom"; 
+            }
+            else if(!$prenom && validationChar(($nom))){
+                $nom = mb_strtoupper($nom);
+                return "votre nom est: $nom";
+            }
+            
+            return "Information incorrectes";
         }
-        else if(!$prenom && validationChar(($nom))){
-            $nom = mb_strtoupper($nom);
-            return "votre nom est: $nom";
-        }
-        
-        return "Information incorrectes";
-    }
     function validationChar($var): bool
-    {   
-        
-        if($var && preg_match('/^[A-Za-zÀ-ÿ]+(?: [A-Za-zÀ-ÿ]+)*$/', $var)){
-            return true;
+        {   
+            
+            if($var && preg_match('/^[A-Za-zÀ-ÿ]+(?: [A-Za-zÀ-ÿ]+)*$/', $var)){
+                return true;
+            }
+            else{
+                return false;
+            }
         }
-        else{
-            return false;
-        }
-    }
 ?>
